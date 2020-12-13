7a9305e631b8db5138f1f717ac6e87236823fe6c
Hanke Zhang <541833341@qq.com>
2020-12-14 01:24:25 +0800

    small changes

--- cli.py
+++ cli.py
@@ -1,11 +1,22 @@
 from command import commandInvoker
 
-if __name__ == "__main__":
 
-    while True:
-        command = input("Input your command:\n")
-        print("-------------------")
-        if command == "exit":
-            break
-        res = commandInvoker.execute(command)
-        print(res)
+class Client(object):
+    def __init__(self):
+        self.invoker = commandInvoker
+        print("server ready")
+
+    def run(self):
+        while True:
+            command = input("Input your command:\n")
+            print("-------------------")
+            if command == "exit":
+                break
+            res = commandInvoker.execute(command)
+            print(res)
+
+
+if __name__ == "__main__":
+    c = Client()
+    c.run()
+
